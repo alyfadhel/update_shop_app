9a5db0edf0ad90876897f7b839a534c9ee2ae9a9
alyfa <alyfadel67@gmail.com>
2022-12-27 09:45:32 +0200

    add conditional builder to products details page

--- lib/features/home/presentation/screens/productsDetails.dart
+++ lib/features/home/presentation/screens/productsDetails.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_now/core/layout/cubit/cubit.dart';
@@ -36,76 +37,82 @@ class _ProductsDetailsState extends State<ProductsDetails> {
           var cubit = HomeCubit.get(context);
           return Scaffold(
             appBar: AppBar(),
-            body: Padding(
-              padding: const EdgeInsets.all(AppPadding.p20),
-              child: SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CarouselSlider(
-                        items: widget.products.images.map(
-                                (e) =>Image(
-                                  width: double.infinity,
-                                  fit: BoxFit.contain,
-                                  image: NetworkImage(e),
-                                ),
-                        ).toList(),
-                        options: CarouselOptions(
-                          height: 400,
-                          initialPage: 0,
-                          enableInfiniteScroll: false,
-                          viewportFraction: 1.0,
-                          scrollPhysics: const BouncingScrollPhysics(),
-                          autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                          autoPlayCurve: Curves.fastOutSlowIn,
-                          enlargeCenterPage: true,
-                          onPageChanged: (index, reason) {
-                            setState(() {
-                              current = index;
-                            });
-                          },
-                          scrollDirection: Axis.horizontal,
-                        )),
-                    const SizedBox(
-                      height: AppSize.s10,
-                    ),
-                    Text(
-                       widget.products.price.round().toString(),
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: ColorManager.bTwitter
-                      ),
-                    ),
-                    const SizedBox(
-                      height: AppSize.s10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+            body: ConditionalBuilder(
+                condition: state is! GetProductsDetailsLoadingState,
+                builder: (context) => Padding(
+                  padding: const EdgeInsets.all(AppPadding.p20),
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        AnimatedSmoothIndicator(
-                          activeIndex: current,
-                          count: widget.products.images.length,
-                          effect: const JumpingDotEffect(
-                              dotHeight: 10,
-                              dotWidth: 10,
-                              jumpScale: .7,
-                              verticalOffset: 20,
-                              activeDotColor: Colors.indigo,
-                              dotColor: Colors.grey,
+                        CarouselSlider(
+                            items: widget.products.images.map(
+                                  (e) =>Image(
+                                width: double.infinity,
+                                fit: BoxFit.contain,
+                                image: NetworkImage(e),
+                              ),
+                            ).toList(),
+                            options: CarouselOptions(
+                              height: 400,
+                              initialPage: 0,
+                              enableInfiniteScroll: false,
+                              viewportFraction: 1.0,
+                              scrollPhysics: const BouncingScrollPhysics(),
+                              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                              autoPlayCurve: Curves.fastOutSlowIn,
+                              enlargeCenterPage: true,
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  current = index;
+                                });
+                              },
+                              scrollDirection: Axis.horizontal,
+                            )),
+                        const SizedBox(
+                          height: AppSize.s10,
+                        ),
+                        Text(
+                          widget.products.price.round().toString(),
+                          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              color: ColorManager.bTwitter
                           ),
+                        ),
+                        const SizedBox(
+                          height: AppSize.s10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            AnimatedSmoothIndicator(
+                              activeIndex: current,
+                              count: widget.products.images.length,
+                              effect: const JumpingDotEffect(
+                                dotHeight: 10,
+                                dotWidth: 10,
+                                jumpScale: .7,
+                                verticalOffset: 20,
+                                activeDotColor: Colors.indigo,
+                                dotColor: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: AppSize.s30,
+                        ),
+                        Text(
+                          widget.products.description,
+                          style: Theme.of(context).textTheme.titleLarge,
                         ),
                       ],
                     ),
-                    const SizedBox(
-                      height: AppSize.s30,
-                    ),
-                    Text(
-                      widget.products.description,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                fallback: (context) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
             ),
           );
         },
